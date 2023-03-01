edd3eaaac06e867f73a505f04a6a2a5abf60c4d7
harirathod <h4ri.rathod@gmail.com>
2023-03-01 12:00:56 +0000

    Reverse text in display label when user hovers over asterisk

--- src/main/java/com/phone/phonegui/PhoneGui.java
+++ src/main/java/com/phone/phonegui/PhoneGui.java
@@ -3,6 +3,8 @@ package com.phone.phonegui;
 import javafx.application.Application;
 import javafx.scene.*;
 import javafx.scene.control.*;
+import javafx.scene.input.MouseDragEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
@@ -56,51 +58,73 @@ public class PhoneGui extends Application {
 
     private void createPhone(GridPane grid)
     {
-        List<VBox> numbers = new ArrayList<>();
         VBox one = new VBox();
         one.getChildren().add(new Label("1"));
-        one.setOnMouseClicked(event -> displayLabel.setText(displayLabel.getText() + "1"));
         VBox two = new VBox();
         two.getChildren().addAll(new Label("ABC"), new Label("2"));
-        two.setOnMouseClicked(event -> displayLabel.setText(displayLabel.getText() + "2"));
         VBox three = new VBox();
         three.getChildren().addAll(new Label("DEF"), new Label("3"));
-        three.setOnMouseClicked(event -> displayLabel.setText(displayLabel.getText() + "3"));
         VBox four = new VBox();
         four.getChildren().addAll(new Label("GHI"), new Label("4"));
-        four.setOnMouseClicked(event -> displayLabel.setText(displayLabel.getText() + "4"));
         VBox five = new VBox();
         five.getChildren().addAll(new Label("JKL"), new Label("5"));
-        five.setOnMouseClicked(event -> displayLabel.setText(displayLabel.getText() + "5"));
         VBox six = new VBox();
         six.getChildren().addAll(new Label("MNO"),new Label("6"));
-        six.setOnMouseClicked(event -> displayLabel.setText(displayLabel.getText() + "6"));
         VBox sev = new VBox();
         sev.getChildren().addAll(new Label("PQRS"), new Label("7"));
-        sev.setOnMouseClicked(event -> displayLabel.setText(displayLabel.getText() + "7"));
         VBox eight = new VBox();
         eight.getChildren().addAll(new Label("TUV"), new Label("8"));
-        eight.setOnMouseClicked(event -> displayLabel.setText(displayLabel.getText() + "8"));
         VBox nine = new VBox();
         nine.getChildren().addAll(new Label("WXYZ"),new Label("9"));
-        nine.setOnMouseClicked(event -> displayLabel.setText(displayLabel.getText() + "9"));
-
-
-
-        VBox ast = new VBox();
-        ast.getChildren().addAll(new Label("*"));
-        ast.getChildren().get(0).setId("asterisk");
         VBox zero = new VBox();
         zero.getChildren().addAll(new Label("0"));
+
+        List<VBox> numbers = new ArrayList<>();
+        numbers.add(one);
+        numbers.add(two);
+        numbers.add(three);
+        numbers.add(four);
+        numbers.add(five);
+        numbers.add(six);
+        numbers.add(sev);
+        numbers.add(eight);
+        numbers.add(nine);
+        numbers.add(zero);
+        // add event listener so when a keypad button is clicked, update the displayLabel with the text/number in the
+        // keypad button clicked
+        for(VBox vbox : numbers) {
+            Label label = (Label) vbox.getChildren().get(vbox.getChildren().size() - 1);
+            vbox.setOnMouseClicked(event -> displayLabel.setText(displayLabel.getText() + label.getText()));
+        }
+
+        VBox ast = new VBox();
+        ast.getChildren().add(new Label("*"));
+        ast.setId("asterisk-button");
+        ast.setOnMouseEntered(this::reverseDisplayLabel);
+        ast.setOnMouseExited(this::reverseDisplayLabel);
+
         VBox hash = new VBox();
         hash.getChildren().addAll(new Label("#"));
+        hash.setOnMouseClicked(event -> displayLabel.setText(""));
 
         grid.addRow(0, one, two, three);
         grid.addRow(1, four, five, six);
         grid.addRow(2, sev, eight, nine);
         grid.addRow(3, ast, zero, hash);
         for(Node node : grid.getChildren()) {
-            node.getStyleClass().add("keypad");
+            node.getStyleClass().add("keypad-button");
         }
+    }
+
+    /**
+     * Reverse the text in the display label.
+     * @param mouseEvent The event generated when the mouse is dragged over.
+     */
+    private void reverseDisplayLabel(MouseEvent mouseEvent) {
+        String temp = "";
+        for (int i = displayLabel.getText().length() - 1; i >= 0; i--) {
+            temp += displayLabel.getText().substring(i, i + 1);
+        }
+        displayLabel.setText(temp);
     }
 }
