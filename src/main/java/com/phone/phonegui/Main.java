fc743bbb6177f2a12f47e0d1336d513a1d4acf4a
harirathod <h4ri.rathod@gmail.com>
2023-03-07 10:26:13 +0000

    Create file to hold calculator and phone

---
+++ src/main/java/com/phone/phonegui/Main.java
@@ -0,0 +1,19 @@
+package com.phone.phonegui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * This is the main class that shows both the phone and the calculator.
+ */
+public class Main extends Application {
+
+    /**
+     * This is the entry point into the application.
+     * @param stage
+     */
+    @Override
+    public void start(Stage stage) {
+
+    }
+}
