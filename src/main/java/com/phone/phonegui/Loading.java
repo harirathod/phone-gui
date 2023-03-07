f95d31fdbc6122580b24d630721edd06ff9aa738
harirathod <h4ri.rathod@gmail.com>
2023-03-07 13:47:41 +0000

    Create loading screen class

--- src/main/java/com/phone/phonegui/Loading.java
+++ src/main/java/com/phone/phonegui/Loading.java
@@ -1,2 +1,35 @@
-package com.phone.phonegui;public class Loading {
+package com.phone.phonegui;
+
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+
+public class Loading extends BorderPane {
+
+    protected Label loadingLabel;
+    protected Text text;
+
+    public Loading()
+    {
+        text = new Text();
+        loadingLabel = new Label();
+        createLoadingScreen();
+    }
+
+    private void createLoadingScreen()
+    {
+        text.setText("Loading...");
+        text.setFont(Font.font("Open Sans", 32));
+        text.setFill(Color.WHITE);
+        loadingLabel.setGraphic(text);
+        this.setCenter(loadingLabel);
+
+        this.setMaxHeight(Double.MAX_VALUE);
+        this.setMaxWidth(Double.MAX_VALUE);
+        this.setBackground(Background.fill(Color.BLACK));
+    }
 }
