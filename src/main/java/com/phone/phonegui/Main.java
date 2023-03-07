fe825859568fa5dd0374a0e463ee7854e157403b
harirathod <h4ri.rathod@gmail.com>
2023-03-07 12:48:50 +0000

    Add phone and calculator to main class

--- src/main/java/com/phone/phonegui/Main.java
+++ src/main/java/com/phone/phonegui/Main.java
@@ -1,19 +1,37 @@
 package com.phone.phonegui;
 
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import javafx.scene.*;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.stage.*;
 
 /**
  * This is the main class that shows both the phone and the calculator.
  */
 public class Main extends Application {
-
+    private CalculatorDisplay calculator = new CalculatorDisplay();
+    private PhoneDisplay phone = new PhoneDisplay();
     /**
      * This is the entry point into the application.
      * @param stage
      */
     @Override
     public void start(Stage stage) {
+        StackPane root = new StackPane();
+        root.getChildren().addAll(calculator, phone);
+        phone.getSwitchDisplayLabel().setOnMouseClicked(this::showCalculator);
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add("/style.css");
+        stage.setScene(scene);
+        stage.sizeToScene();
+        stage.show();
+    }
+
+
+    private void showCalculator(MouseEvent event) {
 
     }
 }
